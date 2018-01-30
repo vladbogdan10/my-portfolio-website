5038404735dfb102c0782a397370b397aa195ea7
Vlad Bogdan <vlad.bogdan90@gmail.com>
2018-01-30 12:09:05 +0100

    Changed footer layout for mobile and desktop

---
+++ config.rb
@@ -0,0 +1,9 @@
+http_path = "/"
+        css_dir = "/"
+        sass_dir = "sass"
+        images_dir = "img"
+        javascripts_dir = "javascripts"
+        output_style = :compressed
+        relative_assets=true
+        line_comments = false
+        
