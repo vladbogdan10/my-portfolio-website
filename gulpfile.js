5a487a9db33bc43210a48d140a842c788c04a946
Vlad Bogdan <vlad.bogdan90@gmail.com>
2018-04-29 16:00:14 +0200

    Added sourcemaps and autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,50 @@
-var gulp      = require('gulp'),
-  browserSync = require('browser-sync').create(),
-  sass        = require('gulp-sass');
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    browserSync = require('browser-sync').create(),
+    sourcemaps = require('gulp-sourcemaps'),
+    autoprefixer = require('gulp-autoprefixer');
+
+var input = './sass/**/*.scss',
+    output = './public/assets/css';
+
+var sassOptions = {
+  errLogToConsole: true,
+  outputStyle: 'expanded'
+};
+
+var autoprefixerOptions = {
+  browsers: ['last 2 versions', '> 5%']
+};
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function () {
+  return gulp.src(input)
+  .pipe(sourcemaps.init())
+  .pipe(sass(sassOptions).on('error', sass.logError))
+  // .pipe(autoprefixer(autoprefixerOptions))
+  .pipe(sourcemaps.write('../../../maps'))
+  .pipe(gulp.dest(output))
+  .pipe(browserSync.stream());
+});
 
 // Static Server + watching scss/html files
 gulp.task('serve', ['sass'], function () {
   browserSync.init({
     server: {
-      baseDir: "./"
+      baseDir: "./public"
     }
   });
-
-  gulp.watch("./sass/**/*.scss", ['sass']);
-  gulp.watch("./*.html").on('change', browserSync.reload);
+  
+  gulp.watch(input, ['sass']);
+  gulp.watch("./public/*.html").on('change', browserSync.reload);
 });
 
-// Compile sass into CSS & auto-inject into browsers
-gulp.task('sass', function () {
-  return gulp.src('./sass/**/*.scss')
-    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-    .pipe(gulp.dest('./'))
-    .pipe(browserSync.stream());
+gulp.task('prod', function () {
+  return gulp
+    .src(input)
+    .pipe(sass({ outputStyle: 'compressed' }))
+    .pipe(autoprefixer(autoprefixerOptions))
+    .pipe(gulp.dest(output));
 });
 
 gulp.task('default', ['serve']);
