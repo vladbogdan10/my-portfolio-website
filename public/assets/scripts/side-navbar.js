73e24ae8f2bb34daa4bf814c7f2b3f311d7d6839
Vlad Bogdan <vlad.bogdan90@gmail.com>
2018-05-11 11:23:29 +0200

    added side navigation

---
+++ public/assets/scripts/side-navbar.js
@@ -0,0 +1,46 @@
+(function (window, document) {
+
+  var layout     = document.querySelector('.navbar'),
+      menu       = document.querySelector('#menu'),
+      burgerMenu = document.querySelector('.burger-menu'),
+      content    = document.querySelector('.main');
+
+  function toggleClass(element, className) {
+    var classes = element.className.split(/\s+/),
+      length = classes.length,
+      i = 0;
+
+    for (; i < length; i++) {
+      if (classes[i] === className) {
+        classes.splice(i, 1);
+        break;
+      }
+    }
+    // The className is not found
+    if (length === classes.length) {
+      classes.push(className);
+    }
+
+    element.className = classes.join(' ');
+  }
+
+  function toggleAll(e) {
+    var active = 'active';
+
+    e.preventDefault();
+    toggleClass(layout, active);
+    toggleClass(menu, active);
+    toggleClass(burgerMenu, active);
+  }
+
+  burgerMenu.onclick = function (e) {
+    toggleAll(e);
+  };
+
+  content.onclick = function (e) {
+    if (menu.className.indexOf('active') !== -1) {
+      toggleAll(e);
+    }
+  };
+
+}(this, this.document));
