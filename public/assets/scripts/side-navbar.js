86be4379c355bf231197e0866b580c4adc2155b5
Vlad Bogdan <vlad.bogdan90@gmail.com>
2018-06-12 17:25:30 +0200

    changed/removed function param. name

--- public/assets/scripts/side-navbar.js
+++ public/assets/scripts/side-navbar.js
@@ -1,44 +1,38 @@
-(function (window, document) {
-
-  var menu       = document.querySelector('.side-navbar__container'),
-      burgerMenu = document.querySelector('.side-navbar__button'),
-      content    = document.querySelector('.main');
+(function () {
+  var menu = document.querySelector('.side-navbar__container'),
+    burgerMenu = document.querySelector('.side-navbar__button'),
+    content = document.querySelector('.main');
 
   function toggleClass(element, className) {
     var classes = element.className.split(/\s+/),
-      length = classes.length,
-      i = 0;
+      length = classes.length;
 
-    for (; i < length; i++) {
+    for (var i = 0; i < length; i++) {
       if (classes[i] === className) {
         classes.splice(i, 1);
         break;
       }
     }
-    // The className is not found
     if (length === classes.length) {
       classes.push(className);
     }
-
+    
     element.className = classes.join(' ');
   }
 
-  function toggleAll(e) {
+  function toggleAll() {
     var active = 'is-active';
-
-    e.preventDefault();
     toggleClass(menu, active);
     toggleClass(burgerMenu, active);
   }
 
-  burgerMenu.onclick = function (e) {
-    toggleAll(e);
+  burgerMenu.onclick = function (el) {
+    toggleAll(el);
   };
 
-  content.onclick = function (e) {
+  content.onclick = function (el) {
     if (menu.className.indexOf('is-active') !== -1) {
-      toggleAll(e);
+      toggleAll(el);
     }
   };
-
-}(this, this.document));
+}());
